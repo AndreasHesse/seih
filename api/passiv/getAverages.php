ca33d95491dc94ec67a467e097065bee366aa90a
Jan-Erik Revsbech <janerik@moc.net>
2014-04-07 08:31:53 +0200

    [FEATURE] Add caching to passiv getAverages service

--- api/passiv/getAverages.php
+++ api/passiv/getAverages.php
@@ -32,15 +32,27 @@ class AverageAPI extends ApiBaseClass {
 		$startTime = DateTime::createFromFormat('U', $startTimestamp);
 		$endTime = DateTime::createFromFormat('U', $endTimestamp);
 
-		$hourConstraint = '1=1';
-		if ( $startHour <= $endHour) {
-			$hourConstraint = sprintf('hour >= %d AND hour < %d', $startHour, $endHour);
-		} else {
-			$hourConstraint = sprintf('(hour >= %d OR hour < %d)', $startHour, $endHour);
-		}
+		$noCache = (isset($_GET['noCache']) && intval($_GET['noCache'])=== 1) ? TRUE : FALSE;
+		$rendertimeStart = microtime(TRUE);
 		try {
 
-			$query = sprintf('SELECT AVG(averageValue) as average, STDDEV(averageValue) as stdDeviation from hourly where homeId=%d and sensorName="%s" AND %s AND date > "%s" AND date < "%s"', $homeId, $sensorName, $hourConstraint, $startTime->format('Y-m-d H:i'), $endTime->format('Y-m-d H:i'));
+			$hash = $this->calculateCacheHash(array(
+				'dataset' => 'passivAverage',
+				'startTime' => $startTime->format('U'),
+				'endTime' => $endTime->format('U'),
+				'homeID' => $homeId,
+				'sensorName' => $sensorName,
+				'startHour' => $startHour,
+				'endHour' => $endHour
+			));
+			if ($noCache == FALSE && $cachedResult = $this->findFromCache($hash)) {
+				$source = 'cache';
+				$average = $cachedResult;
+			} else {
+				list($source, $average) = $this->getAverageFromMySQLHourlyAverage($homeId, $sensorName, $startTime, $endTime, $startHour, $endHour);
+				$this->writeToCache($hash, $average);
+			}
+
 			$result = array(
 				'statusCode' => 200,
 				'startHour' => $startHour,
@@ -48,19 +60,43 @@ class AverageAPI extends ApiBaseClass {
 				'startTime' => $startTime->format('d/m-Y H:i'),
 				'endTime' => $endTime->format('d/m-Y H:i'),
 				'sensorName' => $sensorName,
-				'homeId' => $homeId
+				'homeId' => $homeId,
+				'source' => $source,
+				'average' => $average
 			);
-			foreach ($this->dbHandle->query($query) as $row) {
-				$result['average'] = floatval($row['average']);
-				$result['standardDeviation'] = floatval($row['stdDeviation']);
-			}
+
+
+			$rendertimeEnd = microtime(TRUE);
+			$result['querytimeInSeconds'] = $rendertimeEnd - $rendertimeStart;
+
 			print json_encode($result);
 		} catch (Exception $e) {
 			$this->renderError('Unable to connect to database');
 		}
-
-
 	}
+
+	/**
+	 * @param $homeId
+	 * @param $sensorName
+	 * @param $startTime
+	 * @param $endTime
+	 * @param $startHour
+	 * @param $endHour
+	 */
+	protected function getAverageFromMySQLHourlyAverage($homeId, $sensorName, $startTime, $endTime, $startHour, $endHour) {
+		$hourConstraint = '';
+		if ( $startHour <= $endHour) {
+			$hourConstraint = sprintf('hour >= %d AND hour < %d', $startHour, $endHour);
+		} else {
+			$hourConstraint = sprintf('(hour >= %d OR hour < %d)', $startHour, $endHour);
+		}
+		$query = sprintf('SELECT AVG(averageValue) as average from hourly where homeId=%d and sensorName="%s" AND %s AND date > "%s" AND date < "%s"', $homeId, $sensorName, $hourConstraint, $startTime->format('Y-m-d H:i'), $endTime->format('Y-m-d H:i'));
+		foreach ($this->dbHandle->query($query) as $row) {
+			$average = floatval($row['average']);
+		}
+		return array('hourlyAverage', $average);
+	}
+
 }
 
 $API = new AverageAPI();
