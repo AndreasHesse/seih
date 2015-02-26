2ce906dca3039fd41b927d349d16277eac064e86
ssh-user <seih@morstup.pil.dk>
2015-02-26 13:08:39 +0000

    close to reopening on morstup

--- conf/settings.php
+++ conf/settings.php
@@ -1,24 +1,28 @@
 <?php
 
 $conf = array(
-		'mongo' => array(
-				'hostname' => 'mongodb://localhost:27017',
-				"username" => "seih_u",
-				"password" => "yzxjeumf41",
-				'database' => 'seih'
-		),
-		'typo3_db' => array(
-				'hostname' => 'localhost',
-				'database' => 'seih',
-				'username' => 'seih_u',
-				'password' => '8zxt358eez'
-		),
-		'mysql' => array(
-				'hostname' => 'localhost',
-				'database' => 'aggregated_data',
-				'username' => 'seih_u',
-				'password' => '8zxt358eez'
-		)
+	'mongo' => array(
+		'hostname' => 'mongodb://91.223.235.136:27017',
+		"username" => "seih_u",
+		"password" => "yzxjeumf41",
+		'database' => 'seih'
+	),
+	'typo3_db' => array(
+		'hostname' => 'localhost',
+		'database' => 'seih',
+		'username' => 'seih_u',
+		'password' => '8zxt358eez'
+	),
+	'mysql' => array(
+		'hostname' => 'localhost',
+		'database' => 'aggregated_data',
+		'username' => 'seih_u',
+		'password' => '8zxt358eez'
+	),
+	'cache' => array(
+		'ttl' => 86400,
+		'cacheDir' => '/dana/data/seih.dk/docs/mithjem/cache'
+	)
 );
 
 return $conf;
